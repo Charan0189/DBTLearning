84854ccb3e064219198a8116a3cad10266eb24cc
ramcharan.s@gathi.com <ramcharan.s@gathi.com>
2022-04-13 16:25:56 +0000

    training

--- models/integration/integration2.sql
+++ models/integration/integration2.sql
@@ -1,30 +1,80 @@
- with orders as  (
+with orders as  (
+
     select * from {{ ref('stg_orders' )}}
+
 ),
+
+
 
 payments as (
+
     select * from {{ ref('stg_payments') }}
+
 ),
+
+
 
 order_payments as (
+
     select
+
         order_id,
-        sum(case when status = 'success' then amount end) as amount
+
+        payment_method,
+
+        sum(amount) as amount,
+
+        created_at
+
+
 
     from payments
-    group by 1
+
+    group by 1,2,4
+
+    
+
 ),
+
+
 
 final as (
 
+
+
     select
+
         orders.order_id,
+
         orders.customer_id,
-        orders.order_date,
-        coalesce(order_payments.amount, 0) as amount
+
+        orders.status,
+
+        order_payments.payment_method,
+
+        order_payments.amount,
+
+        order_payments.created_at
+
+
 
     from orders
+
     left join order_payments using (order_id)
+
+    where 
+    payment_method = '{{ var("payment_method") }}'   
+    and status = '{{ var("status") }}'
+
+    and amount > {{ var("amount") }} 
+    and created_at >= '{{ var("created") }}'
+
+    order by 2
+
+    
+
 )
+
+
 
 select * from final
