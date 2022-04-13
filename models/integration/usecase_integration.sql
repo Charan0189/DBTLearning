19cab761f26211eafdc6f86b4a3c9296c40d9571
ramcharan.s@gathi.com <ramcharan.s@gathi.com>
2022-04-13 09:56:53 +0000

    training

--- models/integration/usecase_integration.sql
+++ models/integration/usecase_integration.sql
@@ -2,4 +2,4 @@ select c.FIRST_NAME, O.ORDER_DATE, O.STATUS, p.PAYMENTMETHOD, p.AMOUNT
 from 
 {{ source('usecase1', 'customers') }} c left join {{source('usecase1', 'orders')}} o on c.ID = o.ID
 left join {{ source('usecase2', 'payment') }} p on c.ID = p.ID
-where o.STATUS = 'completed' and p.PAYMENTMETHOD = 'bank_transfer' and p.AMOUNT > 1200
+where o.STATUS = 'completed' and p.PAYMENTMETHOD = 'bank_transfer' and p.AMOUNT > 1200 and o.ORDER_DATE >= '2018-01-20'
---
+++ models/load/usecase_load.sql
@@ -0,0 +1,1 @@
+select * from {{ref('integration1')}}
