9ec5612ef150909103904cee5c78c9ad68892fc3
ramcharan.s@gathi.com <ramcharan.s@gathi.com>
2022-04-19 18:56:32 +0000

    training

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -4,7 +4,7 @@ select
     trim(initcap(paymentmethod)) as payment_method,
     ROUND(amount, 2) as amount,
     created as created_at,
-    status
+    initcap(status) as payment_status
     -- amount is stored in cents, convert it to dollars
  -- {{ cents_to_dollars('amount', 4) }} as amount,
    -- created as created_at
